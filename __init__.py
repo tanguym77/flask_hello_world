3b7c6494e7a4f00be8963b4217a3f098650e38b5
tanguym77 <99647642+tanguym77@users.noreply.github.com>
2024-01-25 14:25:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 @app.route('/test')
 def hello_world():
     return render_template('hello.html')
+
+@app.route('/fr/')
+def bonjour():
+    return "Bonjour tout le monde !";
+
                                                                                           
 if __name__ == "__main__":
   app.run(debug=True)
