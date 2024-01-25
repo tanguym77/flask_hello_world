ffbfd26a53d1dcc5390039e7f623a71572674507
tanguym77 <99647642+tanguym77@users.noreply.github.com>
2024-01-25 14:16:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
-@app.route('/')
+@app.route('/test')
 def hello_world():
     return render_template('hello.html')
-                                                                                                                                       
+                                                                                          
 if __name__ == "__main__":
   app.run(debug=True)
